60908332bd6157fbffc06619463490c74cd70e02
MrazFilip <95131736+MrazFilip@users.noreply.github.com>
2022-04-21 20:07:53 +0200

    Add files via upload

---
+++ RaFilDaBackupService/BackupJob.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace RaFilDaBackupService
+{
+    public class BackupJob : IJob
+    {
+        private readonly ILogger<BackupJob> _logger;
+        public BackupJob(ILogger<BackupJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            _logger.LogInformation("Backup: " + dataMap.GetString("jobName") + " STARTED");
+
+            List<string> oldLogs = new List<string>();
+
+            if (Backup(dataMap.GetInt("jobType"),
+                      dataMap.GetString("jobSource"),
+                      dataMap.GetString("jobDestination"),
+                      dataMap.GetInt("jobRetention"),
+                      dataMap.GetInt("jobPackages"),
+                      dataMap.GetString("jobName")))
+            {
+                oldLogs.Add(DateTime.Now + " SUCCESSFULLY BACKUPED UP: " + dataMap.GetString("jobName"));
+                _logger.LogInformation("Backup: " + dataMap.GetString("jobName") + " COMPLETED");
+            }   
+            else
+            {
+                oldLogs.Add(DateTime.Now + " ERROR IN BACKUP: " + dataMap.GetString("jobName"));
+                _logger.LogInformation("Backup: " + dataMap.GetString("jobName") + " FAILED");   
+            }
+
+            StreamReader sr = new StreamReader(@"..\log.txt");
+            while (!sr.EndOfStream)
+            {
+                oldLogs.Add(sr.ReadLine());
+            }
+            sr.Close();
+
+            //TODO: ADD SENDING REPORTS TO SERVER
+            if (false) //TODO: ADD CONNECTING SERVER
+            {
+                //TODO: SEND ALL LOGS
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(@"..\log.txt");
+                foreach (string log in oldLogs)
+                {
+                    sw.WriteLine(log);
+                }
+                sw.Close();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public static bool Backup(int type, string source, string destination, int retention, int packages, string name)
+        {
+            string typeBackup = "";
+            switch (type)
+            {
+                case 0:
+                    typeBackup = "FULL";
+                    break;
+                case 1:
+                    typeBackup = "DIFF";
+                    break;
+                case 2:
+                    typeBackup = "INC";
+                    break;
+            }
+
+            try
+            {
+                StartBackup(typeBackup, source, destination, retention, packages, name);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+        }
+
+        private static void StartBackup(string typeBackup, string pathSource, string pathDestination, int retention, int packages, string name)
+        {
+            BackupTools bt = new BackupTools(retention, packages);
+
+            pathDestination = pathDestination + @"\" + name + @"\";
+
+            bt.NewLists();
+
+            typeBackup += "_BACKUP";
+
+            string infoPath = pathDestination + @$"\{typeBackup}\";
+
+            Directory.CreateDirectory(infoPath);
+
+            if (!bt.CheckForFile(infoPath))
+                bt.UpdateFile(infoPath, DateTime.MinValue.ToString(), bt.RETENTION, typeBackup == "FULL_BACKUP" ? 1 : bt.PACKAGES, "1");
+
+            pathDestination = pathDestination + @$"\{typeBackup}\" + "BACKUP_" + bt.GetInfo(infoPath)[3] + pathSource.Remove(0, pathSource.LastIndexOf("\\"));
+
+            Directory.CreateDirectory(pathDestination);
+
+            if (typeBackup != "FULL_BACKUP")
+            {
+                if (Convert.ToInt32(bt.GetInfo(infoPath)[2]) < bt.PACKAGES)
+                    pathDestination += @"\PACKAGE_" + ((Convert.ToInt32(bt.GetInfo(infoPath)[2]) - 6) * -1) + "\\";
+                else
+                    pathDestination += @"\FULL\";
+                Directory.CreateDirectory(pathDestination);
+            }
+
+            DateTime snapshot = DateTime.Parse(bt.GetInfo(infoPath)[0]);
+
+            foreach (string dir in Directory.GetDirectories(pathSource, "*", SearchOption.AllDirectories))
+            {
+                if (new DirectoryInfo(dir).LastWriteTime <= snapshot)
+                    continue;
+                bt.Dirs.Add(dir);
+                Directory.CreateDirectory(dir.Replace(pathSource, pathDestination));
+            }
+
+            foreach (string file in Directory.GetFiles(pathSource, "*.*", SearchOption.AllDirectories))
+            {
+                if (new FileInfo(file).LastWriteTime <= snapshot)
+                    continue;
+                bt.Files.Add(file);
+                File.Copy(file, file.Replace(pathSource, pathDestination), true);
+            }
+
+            if (int.Parse(bt.GetInfo(infoPath)[2]) == 5 || typeBackup != "DIFF_BACKUP")
+                bt.UpdateFile(infoPath, DateTime.Now.ToString(), Convert.ToInt32(bt.GetInfo(infoPath)[1]), Convert.ToInt32(bt.GetInfo(infoPath)[2]) - 1, bt.GetInfo(infoPath)[3]);
+            else if (typeBackup == "DIFF_BACKUP")
+                bt.UpdateFile(infoPath, bt.GetInfo(infoPath)[0], Convert.ToInt32(bt.GetInfo(infoPath)[1]), Convert.ToInt32(bt.GetInfo(infoPath)[2]) - 1, bt.GetInfo(infoPath)[3]);
+
+            if (bt.GetInfo(infoPath)[2] == "0")
+            {
+                bt.Pack(infoPath, typeBackup);
+            }
+
+            bt.LogFiles(pathDestination);
+        }
+    }
+}
---
+++ RaFilDaBackupService/BackupTools.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RaFilDaBackupService
+{
+    public class BackupTools
+    {
+        public BackupTools(int retention, int packages)
+        {
+            RETENTION = retention;
+            PACKAGES = packages;
+        }
+
+        public int RETENTION { get; set; }
+        public int PACKAGES { get; set; }
+        public List<string> Dirs = new List<string>();
+        public List<string> Files = new List<string>();
+        public List<string> NewFiles = new List<string>();
+        public List<string> NewDirs = new List<string>();
+        public void NewLists()
+        {
+            Dirs = new List<string>();
+            Files = new List<string>();
+            NewDirs = new List<string>();
+            NewFiles = new List<string>();
+        }
+
+        public void LoadFiles(string path)
+        {
+            using StreamReader sr = new StreamReader(path + @"backup_file_info.txt");
+            {
+                sr.ReadLine();
+                string dir = "";
+                while (dir != "Files:")
+                {
+                    dir = sr.ReadLine();
+                    if (dir != "Files:")
+                        Dirs.Add(dir);
+                }
+                while (!sr.EndOfStream)
+                {
+                    Files.Add(sr.ReadLine());
+                }
+                sr.Close();
+            }
+        }
+
+        public void LogFiles(string path)
+        {
+            using StreamWriter sw = new StreamWriter(path + @"backup_file_info.txt");
+            {
+                sw.WriteLine("Dirs:");
+                foreach (string item in Dirs)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.WriteLine("Files:");
+                foreach (string item in Files)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            }
+        }
+
+        public bool CheckForFile(string path)
+        {
+            if (!File.Exists(path + @"info.txt"))
+                return false;
+            else
+                return true;
+        }
+
+        public void UpdateFile(string path, string snapshot, int retention, int? packages, string number)
+        {
+            //1st line = last snapshot time
+            //2nd line = RETENTION
+            //3rd line = PACKAGES
+            //4th line = number of backup
+
+            using StreamWriter sw = new StreamWriter(path + @"info.txt");
+            {
+                sw.WriteLine(snapshot);
+                sw.WriteLine(retention);
+                sw.WriteLine(packages);
+                sw.WriteLine(number);
+                sw.Close();
+            }
+        }
+
+        public string[] GetInfo(string path)
+        {
+            string[] result = new string[4];
+            int indexer = 0;
+            using StreamReader sr = new StreamReader(path + @"info.txt");
+            {
+                while (!sr.EndOfStream)
+                {
+                    result[indexer] = sr.ReadLine();
+                    indexer++;
+                }
+                sr.Close();
+            }
+            return result;
+        }
+
+        public void Pack(string path, string typeBackup)
+        {
+            int retention = Convert.ToInt32(GetInfo(path)[1]);
+            if (retention == 1)
+            {
+                DeleteOldest(path);
+                retention++;
+            }
+            UpdateFile(path, DateTime.MinValue.ToString(), retention - 1, typeBackup == "FULL_BACKUP" ? 1 : PACKAGES, (Convert.ToInt32(GetInfo(path)[3]) + 1).ToString());
+        }
+
+        public void DeleteOldest(string path)
+        {
+            List<DirectoryInfo> dirs = new List<DirectoryInfo>();
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                dirs.Add(new DirectoryInfo(dir));
+            }
+
+            dirs.Sort((x, y) => x.CreationTime.CompareTo(y.CreationTime));
+            dirs[0].Delete(true);
+        }
+    }
+}
